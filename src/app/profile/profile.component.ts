3bfe991d12a2b4f1e5c835733cbbc8e39390d541
Stéphane ZUCATTI <stephane.zucatti@me.com>
2022-09-20 12:10:36 +0200

    update

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Auth } from '../../services/auth.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -8,13 +7,9 @@ import { Router } from '@angular/router';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  constructor(private auth: Auth, private route: Router) {}
-  session: any = {};
-  user: String = '';
-  ngOnInit(): void {
-    this.session = this.auth.session;
-    this.user = this.session.user.email;
-  }
+  constructor(private auth: Auth) {}
+
+  ngOnInit(): void {}
   async logout() {
     await this.auth.signOut();
     window.location.href = '/';
